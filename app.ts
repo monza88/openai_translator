205ec149e5092e078e47da7f892de72755ef8690
WONSEOK <bloodieye@gmail.com>
2025-07-28 15:51:09 +0900

    cloud run 빌드 버전 1

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import aiRoutes from './routes/ai';
+import 'dotenv/config'; // 환경 변수 로드
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("OpenAI Translator API is running");   
+});
 
 app.use(express.json()); //JSON 파싱 미들웨어
 app.use("/ai", aiRoutes); //ai 경로 등록
 
 export default app;
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
